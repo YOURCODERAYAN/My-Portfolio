8bbe4f7505deb9c0e58627ea2f49bc953e2f6e48
YOURCODERAYAN <ayanwork2003@gmail.com>
2026-05-01 19:42:13 +0530

    Also some background changes in hero sections

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,7 +14,7 @@ function Hero(){
         return(
             <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
                 <HeroText />
-                <Parallax/>
+            { /*  <Parallax/> */ }
 
 
             <figure
