c140ac8c51876db90d51a2051b4556b52b9b0439
YOURCODERAYAN <ayanwork2003@gmail.com>
2026-05-01 19:42:59 +0530

    constants file

---
+++ src/constants/met.js
@@ -0,0 +1,8 @@
+const met =[
+
+    {img : "/assests/car1.jpg" , link :"https://www.google.com" , name :"Ai Resume Analyzer" , tech : "React.js , tailwindcss , react-router , puter.js"},
+    {img : "/assests/picture2.jpg" , link: "https://www.flipkart.com" ,name : "Macbook 3D Experience" , tech:"React.js , gsap , three.js , R3F , css"},
+    {img : "/assests/picture3.jpg" , link: "https://www.amazon.com", name:"Global Terrorism Model" , tech:" Python , Pytorch , matplotlib"}
+]
+
+export default met 
