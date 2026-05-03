ca951008f37857d5e735cd3fbe803cbd709b8c15
YOURCODERAYAN <ayanwork2003@gmail.com>
2026-05-03 23:57:26 +0530

    footer mapped icoons and info from contants file

---
+++ src/constants/mySocials.js
@@ -0,0 +1,17 @@
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/918334904696",
+    icon: "/logos/Whatsapp.png",
+  },
+  {
+    name: "Github",
+    href: "https://github.com/YOURCODERAYAN",
+    icon: "/logos/icons8-github-96.png",
+  },
+  {
+    name: "Wellfound",
+    href: "",
+    icon: "/logos/Wellfound.svg",
+  },
+];
